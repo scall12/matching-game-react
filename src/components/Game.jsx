21f2f128ef68029147d1b25d232f4520c5739c39
Spencer Call <spencergeraldcall@gmail.com>
2020-04-29 15:10:29 -0600

    Create deck of cards based on size of array, with two of each unique value in the array. Only works with even NxN boards. Shuffles the values and assigns them to buttons on the board.

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Card = (props) => {
   const [text, setText] = useState('');
@@ -42,27 +42,56 @@ const Setup = (props) => {
 };
 
 const Board = (props) => {
-  const renderRow = (num) => {
+  const [cards, setCards] = useState([]);
+  const createDeck = (size) => {
+    const uniqueVals = (size * size) / 2;
+    const valsArr = [];
+    for (let i = 0; i < uniqueVals; i++) {
+      valsArr.push(i);
+      valsArr.push(i);
+    }
+    for (let card of valsArr) {
+      const a = valsArr.indexOf(card);
+      const b = valsArr[Math.floor(Math.random() * valsArr.length)];
+
+      let tempVal = card;
+      valsArr[a] = valsArr[b];
+      valsArr[b] = tempVal;
+    }
+    setCards(valsArr);
+  };
+
+  useEffect(() => {
+    createDeck(props.n);
+  }, [props]);
+
+  // useEffect(() => {
+  //   console.log(cards);
+  // }, [cards]);
+
+  const renderRow = (num, deck) => {
     const arr = [];
     for (let i = 0; i < num; i++) {
-      arr.push(<Card key={i} value={i} />);
+      arr.push(<Card key={i} value={deck[0]} />);
+      deck.shift();
     }
     return arr;
   };
 
-  const renderBoard = (num) => {
+  const renderBoard = (num, cards) => {
+    const deck = cards.slice();
     const arr = [];
     for (let i = 0; i < num; i++) {
       arr.push(
         <div id={`row${i}`} key={`row${i}`}>
-          {renderRow(num)}
+          {renderRow(num, deck)}
         </div>
       );
     }
     return arr;
   };
 
-  return <div>{renderBoard(props.n)}</div>;
+  return <div>{renderBoard(props.n, cards)}</div>;
 };
 
 const Game = () => {
