a5f2238f99fd52c3cb7da8fe8b07a9776f304384
Spencer Call <spencergeraldcall@gmail.com>
2020-05-01 13:29:25 -0600

    Trying to only let two cards be flipped for a turn

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Card = (props) => {
   const [text, setText] = useState('');
@@ -8,10 +8,18 @@ const Card = (props) => {
     if (!visible) {
       setText(props.value);
       setVisible(true);
+    } else if (visible) {
     } else {
       setText('');
       setVisible(false);
     }
+    // if (Object.values(props.turn).includes(props.id)) {
+    //   setText(props.value);
+    //   setVisible(true);
+    // } else {
+    //   setText('');
+    //   setVisible(false);
+    // }
   };
 
   return (
