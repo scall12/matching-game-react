896101260a5e70540b2bc676c4451861af8eb48b
Spencer Call <spencergeraldcall@gmail.com>
2020-05-01 15:37:51 -0600

    Add useEffect to compare the cards in the turn. Adjust handleClick logic by storing the card id and value in the turn state object to determine a match.

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,21 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Board from './Board';
 import Setup from './Setup';
 
 const Game = () => {
   const [size, setSize] = useState(4);
-  const [turn, setTurn] = useState({});
+  const [turn, setTurn] = useState({ card1: {}, card2: {} });
 
-  const handleClick = (value) => {
-    if (!turn.card1) {
-      setTurn({ ...turn, card1: value });
-    } else if (!turn.card2 && turn.card1 !== value) {
-      setTurn({ ...turn, card2: value });
-    } else {
-      setTurn({ card1: value });
+  const handleClick = (id, value) => {
+    if (!turn.card1.id) {
+      setTurn({ ...turn, card1: { id, value } });
+    } else if (!turn.card2.id && turn.card1.id !== id) {
+      setTurn({ ...turn, card2: { id, value } });
+    } else if (turn.card1.id && turn.card2.id) {
+      setTurn({ card1: {}, card2: {} });
     }
   };
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      if (turn.card1.value === turn.card2.value && turn.card1.value) {
+        console.log('MATCH');
+        setTurn({ card1: {}, card2: {} });
+      } else if (turn.card1.value !== turn.card2.value && turn.card2.value) {
+        console.log('NO WAY');
+        setTurn({ card1: {}, card2: {} });
+      }
+    }, 500);
+
+    return () => clearTimeout(timeout);
+  }, [turn]);
 
   const changeSize = (val) => {
     setSize(val);
@@ -24,7 +38,7 @@ const Game = () => {
   return (
     <>
       <Setup changeSize={changeSize} />
-      <Board n={size} onClick={(val) => handleClick(val)} turn={turn} />
+      <Board n={size} onClick={handleClick} turn={turn} />
     </>
   );
 };
