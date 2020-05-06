1bacb3d63ed63153547bf6d1aafd8cd043780126
Spencer Call <spencergeraldcall@gmail.com>
2020-05-06 16:43:43 -0600

    Change the timer to show the time in MM:SS

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
 
-const Timer = (props) => {
-  return <div>{props.time}</div>;
+const Timer = ({ time }) => {
+  const seconds = time % 60;
+  const minutes = Math.floor(time / 60);
+
+  const s = seconds.toString();
+
+  let sec;
+  if (s.length <= 1) {
+    const holder = s.split('');
+    holder.unshift('0');
+    sec = holder.join('');
+  }
+
+  return (
+    <div>
+      {minutes}:{sec || seconds} Left
+    </div>
+  );
 };
 
 export default Timer;
