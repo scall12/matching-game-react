b7ac8f0b8aabfb81134c3a5097a690d402f1b8bb
Spencer Call <spencergeraldcall@gmail.com>
2020-05-07 13:16:39 -0600

    Updates

---
+++ precache-manifest.2051f5337d896735bbcc2a6a67495235.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6146bf636c191038fe17f21cd5b63e3",
+    "url": "/matching-game-react/index.html"
+  },
+  {
+    "revision": "b86963236f434d336bfa",
+    "url": "/matching-game-react/static/css/main.7cd8743f.chunk.css"
+  },
+  {
+    "revision": "b40874b9bc39cf0bc01c",
+    "url": "/matching-game-react/static/js/2.c9fa0407.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/matching-game-react/static/js/2.c9fa0407.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b86963236f434d336bfa",
+    "url": "/matching-game-react/static/js/main.f82d8da4.chunk.js"
+  },
+  {
+    "revision": "908088237381af9843d1",
+    "url": "/matching-game-react/static/js/runtime-main.7ee8b9f3.js"
+  }
+]);
